9d631fd8da9e6dc67c1c7cebc6c231bdcd5bf7eb
Keesha Hargrove <kharg14@gmail.com>
2020-10-03 11:34:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
     square.to_s
   end
 
-  get '/say/:number/:phrase' do 
+  get '/say/:number/:phrase'  
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @phrase * @number
